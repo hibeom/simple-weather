948c3cea2b21071ba10becb89cf3d8fae6392512
unknown <hbkim1293@gmail.com>
2021-06-15 23:59:11 +0900

    로딩 화면 구현 및 현재 위치 정보 가져오기
    로딩 화면 및 현재 위치 정보 가져오기
    1.3 Asking for Permissions 까지 진행

---
+++ Loading.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Getting the Simple weather</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: "#d0f2f1",
+  },
+  text: {
+    fontSize: 30,
+    color: "#2d2d2d",
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    fontWeight: "bold",
+  },
+});
